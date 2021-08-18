5785394a5d02ef7907aaf4c28904afee55c3c3d4
Paula Rillo <paularillo@gmail.com>
2021-08-18 15:54:50 -0300

    First commit

---
+++ 4-names.js
@@ -0,0 +1,9 @@
+// local
+const secret = 'SUPER SECRET'
+
+//share
+const john = 'john'
+const peter = 'peter'
+
+// set to export the variables you want to acsess
+module.exports = {john,peter}
--- app.js
+++ app.js
@@ -1,5 +1,5 @@
- console.log(__dirname);
- 
-setInterval(() => {
-    console.log('hello world')
-}, 1000)
+const _ = require('lodash');
+
+const items = [1,[2, [3, [4]]]];
+const newItems = _.flattenDeep(items);
+console.log(newItems);
